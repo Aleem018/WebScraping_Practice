be46c0fcbdd035bf37b26b47c148d085ff885f8c
Aleem Ahmed <ikealeem18@gmail.com>
2026-08-31 08:32:08 +0100

    Initial commit

---
+++ FirstMission/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using HtmlAgilityPack;
+
+HttpClient httpClient = new HttpClient();
+string url = "http://quotes.toscrape.com/";
+
+string htmlContent = await httpClient.GetStringAsync(url);
+
+HtmlDocument htmlDocument = new HtmlDocument();
+htmlDocument.LoadHtml(htmlContent);
+
+var firstQuote = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='quote']/span[@class='text']").InnerText;
+
+var author = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='quote']/span/small[@class='author']").InnerText;
+
+Console.WriteLine($"Quote: {firstQuote}");
+Console.WriteLine($"Author: {author}");
---
+++ SecondMission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using HtmlAgilityPack;
+
+HttpClient client = new HttpClient();
+string url = "http://quotes.toscrape.com/";
+
+var htmlShi = await client.GetStringAsync(url);
+
+HtmlDocument htmlDoc = new HtmlDocument();
+htmlDoc.LoadHtml(htmlShi);
+
+var parentContainer = htmlDoc.DocumentNode.SelectNodes("//div[@class='quote']");
+
+if (parentContainer != null)
+{
+    int counter = 0;
+    foreach (var quote in parentContainer)
+    {
+        var text = quote.SelectSingleNode(".//span[@class='text']").InnerText;
+        var author = quote.SelectSingleNode(".//span/small[@class='author']").InnerText;
+
+        counter += 1;
+
+        Console.WriteLine($"Quote {counter}: {text}");
+        Console.WriteLine($"Author {counter}: {author}");
+    }
+} else
+{
+    Console.WriteLine("Parent container does not exist");
+}
